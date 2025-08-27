a47c8274b668120733a634ed74fbd672602c56e9
PCMA-SOFT <pacema1@gmail.com>
2025-08-27 14:48:03 -0300

    Update: 27-08-2025 14:48

--- _venv.bat
+++ _venv.bat
@@ -1,22 +1,43 @@
 @echo off
 cls
-python -m venv venv && ^
-.\venv\Scripts\activate && ^
-if %errorlevel% neq 0 (
-    echo Failed to create or activate the virtual environment. Please ensure Python is installed and accessible.
-    exit /b %errorlevel%
+if not exist venv (
+    echo Creating venv Activating...
+    python -m venv venv 
+    if %errorlevel% neq 0 (
+        echo Failed to create virtual environment. Please ensure Python is installed and accessible.
+        exit /b %errorlevel%
+    )
+    echo Virtual environment 'venv' created. 
+    .\venv\Scripts\activate 
+    if %errorlevel% neq 0 (
+        echo Failed to activate the existing virtual environment. Please ensure it is not corrupted.
+        exit /b %errorlevel%
+    )
+    echo Virtual environment 'venv' activated.
+    python -m pip install --upgrade pip setuptools wheel 
+    if %errorlevel% neq 0 (
+        echo Failed to upgrade pip, setuptools, or wheel. Please check your Python installation.
+        exit /b %errorlevel%
+    )
+    echo Installing dependencies from requirements.txt...
+    pip install --no-cache-dir -r requirements.txt 
+    if %errorlevel% neq 0 (
+        echo Failed to install dependencies. Please check requirements.txt for issues.
+        exit /b %errorlevel%
+    )
+    echo All dependencies installed successfully.
+) else (
+    echo Activating existing venv...
+    .\venv\Scripts\activate 
+    if %errorlevel% neq 0 (
+        echo Failed to activate the existing virtual environment. Please ensure it is not corrupted.
+        exit /b %errorlevel%
+    )
+    echo Virtual environment 'venv' activated.
 )
-python -m pip install --upgrade pip setuptools wheel && ^
-if %errorlevel% neq 0 (
-    echo Failed to upgrade pip, setuptools, or wheel. Please check your Python installation.
-    exit /b %errorlevel%
-)
-pip install --no-cache-dir -r requirements.txt && ^
-if %errorlevel% neq 0 (
-    echo Failed to install dependencies. Please check requirements.txt for issues.
-    exit /b %errorlevel%
-)
-cls && ^
-echo Virtual environment 'venv' created and activated. Type 'deactivate' to exit. && ^
+
 cmd /k 
+rem cls && ^
+rem echo Virtual environment 'venv' created and activated. Type 'deactivate' to exit. && ^
+
 
