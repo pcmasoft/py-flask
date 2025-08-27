4d67b56e5a3139913c6a3d1a1b33375fa5310948
PCMA-SOFT <pacema1@gmail.com>
2025-08-27 14:33:22 -0300

    Update: 27-08-2025 14:33

--- create_venv.bat
+++ create_venv.bat
@@ -1,4 +1,8 @@
 @echo off
 cls
-python -m venv venv && .\venv\Scripts\activate && python -m pip install --upgrade pip setuptools wheel && pip install -no-cache-dir -r requirements.txt && cmd /k
+python -m venv venv && .\venv\Scripts\activate && ^
+python -m pip install --upgrade pip setuptools wheel && ^
+pip install --no-cache-dir -r requirements.txt && ^
+cmd /k cls &
+echo Virtual environment 'venv' created and activated. Type 'deactivate' to exit.
 
