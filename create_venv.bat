351eeff7b9b51685ed24d324f6e5f9a2338104bc
PCMA-SOFT <pacema1@gmail.com>
2025-08-27 14:25:43 -0300

    Update: 27-08-2025 14:25

--- create_venv.bat
+++ create_venv.bat
@@ -1,4 +1,4 @@
 @echo off
 cls
-python -m venv venv && .\venv\Scripts\activate && python -m pip install --upgrade pip setuptools wheel && pip install -r requirements.txt && cmd /k
+python -m venv venv && .\venv\Scripts\activate && python -m pip install --upgrade pip setuptools wheel && pip install -no-cache-dir -r requirements.txt && cmd /k
 
