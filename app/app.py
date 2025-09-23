e353e175db93589948216b2102342e3c6067354c
PCMA-SOFT <pacema1@gmail.com>
2025-09-23 20:07:01 -0300

    Update: 23-09-2025 20:07

---
+++ app/app.py
@@ -0,0 +1,6 @@
+import flask as fl
+from flask import request, jsonify, render_template, url_for, redirect 
+app = fl.Flask(__name__)
+@app.route('/', methods = ['GET'])
+def home():
+	return "<h1>  Funciona todo ok y en español! </h1>"
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import app.flask_app
-
-if __name__ == "__main__":
-    #app.flask_app.app.run()
-    app.flask_app.app.run(host = '127.0.0.1', port = 5000, threaded = True, debug = True)
-    
-    
+from app.app import *
+if __name__ == "__main__" :
+	#app.run() #release mode
+	app.run(host = '127.0.0.1', port = 5000, threaded = True, debug = True) #debug mode
