a3dc60e21521610c0f2a099db8f130483df4e4ff
PCMA-SOFT <pacema1@gmail.com>
2025-09-23 20:43:24 -0300

    Update: 23-09-2025 20:43

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from app.app import *
-if __name__ == "__main__" :
+#if __name__ == "__main__" :
 	#app.run() #release mode
-	app.run(host = '127.0.0.1', port = 5000, threaded = True, debug = True) #debug mode
+app.run(host = '127.0.0.1', port = 5000, threaded = True, debug = True) #debug mode
