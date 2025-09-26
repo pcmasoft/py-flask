179276ba63c48a96cec786508d19f3620edb5f59
PCMA-SOFT <pacema1@gmail.com>
2025-09-26 10:38:03 -0300

    Update: 26-09-2025 10:38

--- app/app.py
+++ app/app.py
@@ -2,8 +2,11 @@ import flask as fl
 from flask import request, jsonify, render_template, url_for, redirect
 app = fl.Flask(__name__)
 
-@app.route('/', methods=['GET'])
+#@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
-    return "<h1>  Funciona todo ok y en español! </h1>"
+    return render_template("index.html")
+
+
 
 
