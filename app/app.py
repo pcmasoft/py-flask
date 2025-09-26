9846b5ba2b34d94b4c6217b53c11b1c6ec93940c
PCMA-SOFT <pacema1@gmail.com>
2025-09-26 11:13:46 -0300

    Update: 26-09-2025 11:13

--- app/app.py
+++ app/app.py
@@ -2,11 +2,13 @@ import flask as fl
 from flask import request, jsonify, render_template, url_for, redirect
 app = fl.Flask(__name__)
 
-#@app.route('/', methods=['GET'])
+# @app.route('/', methods=['GET'])
+
+
 @app.route('/')
-def home():
-    return render_template("index.html")
-
-
-
-
+def index():
+    dato = {
+        "titulo": "INDEX",
+        "bienvenida": "Mi página de prueba"
+    }
+    return render_template("index.html", dato=dato)
