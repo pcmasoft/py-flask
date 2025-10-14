19b2ecfbf7cb7a292eca1eadce15f4e21e151b40
PCMA-SOFT <pacema1@gmail.com>
2025-10-14 20:13:01 -0300

    Update: 14-10-2025 20:13

--- app/app.py
+++ app/app.py
@@ -18,6 +18,11 @@ def cv():
     return "hola"
 
 
+@app.errorhandler(404)
+def page_not_found():
+    return render_template("404.html"), 404
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         app.run(host='127.0.0.1', port=5000,
