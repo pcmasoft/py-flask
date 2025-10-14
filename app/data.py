c5a877aa5e21e3896303eaa83a9c508e80cfdda8
PCMA-SOFT <pacema1@gmail.com>
2025-10-14 13:31:44 -0300

    Update: 14-10-2025 13:31

--- app/data.py
+++ app/data.py
@@ -1,5 +1,12 @@
 datos = {
     "name": "Pablo",
     "last name": "Medina",
-    "education": ["Python, C++"]
+    "education": [
+        {"skill": "Programming",
+         "Description": "I know to programm in Python and C++",
+         "skill detail": ["Python", "C++", "C#"]},
+        {"skill": "Game development",
+         "Description": "I know development C++ and other frameworks",
+         "skill detail": ["C#", "C++"]}
+    ]
 }
