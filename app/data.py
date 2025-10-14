4e80cf1338b740a8582b91899fe979e97ee9f12a
PCMA-SOFT <pacema1@gmail.com>
2025-10-14 14:39:08 -0300

    Update: 14-10-2025 14:39

--- app/data.py
+++ app/data.py
@@ -4,22 +4,21 @@ datos = {
     "education": [
         {"skill": "Programming",
          "description": "I know to programm in Python and C++",
-         "skill detail": ["Python", "C++", "C#"]},
+         "skill_detail": ["Python", "C++", "C#"]},
         {"skill": "Game development",
          "description": "I know development C++ and other frameworks",
-         "skill detail": ["C#", "C++"]},
+         "skill_detail": ["C#", "C++"]},
         {"skill": "Data Analysis",
          "description": "I know data analysis with Python",
-         "skill detail": ["Python"]},
+         "skill_detail": ["Python"]},
         {"skill": "Web development",
          "description": "With Python and Flask",
-         "skill detail": ["Python", "Flask"]}
+         "skill_detail": ["Python", "Flask"]}
     ],
     "projects": [
         {"project": "Wep App",
          "description": "My personal website built with Flask.",
          "link": "https://github.com/pcmasoft/py-flask"}
-
 
     ]
 }
