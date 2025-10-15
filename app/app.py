f183f9c87fef7b4ab5fc0f3ff1741df466f22e8c
PCMA-SOFT <pacema1@gmail.com>
2025-10-15 08:08:56 -0300

    Update: 15-10-2025  8:08

--- app/app.py
+++ app/app.py
@@ -1,12 +1,11 @@
 import sys
 import app
-from flask import request, jsonify, render_template, url_for, redirect
+from flask import request, jsonify, render_template, url_for, redirect, send_from_directory
 from data import datos
 import app
 from flask import Flask
 
 app = Flask(__name__)
-
 
 
 # @app.route('/', methods=['GET'])
@@ -15,25 +14,35 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", user_data=datos)
 
+
 @app.route("/cv")
 def cv():
     return render_template("cv.html", user_data=datos)
+
+@app.route('/doc/<nombre_archivo>')
+def descargar_archivo(nombre_archivo):
+    # Especifica el directorio de los archivos y el nombre
+    return send_from_directory('doc', nombre_archivo, as_attachment=True)
 
 @app.route("/project")
 def project():
     return render_template("project.html", user_data=datos)
 
+
 @app.route("/experience")
 def experience():
     return render_template("experience.html", user_data=datos)
+
 
 @app.route("/education")
 def education():
     return render_template("education.html", user_data=datos)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
